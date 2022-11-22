3f16fef1234903a71e1cbf425356b4da4a827418
iamcsstudent123 <farihadania@hotmail.com>
2022-11-22 15:37:19 +0500

    add verify import

--- deploy/01-deploy-basic-nft.js
+++ deploy/01-deploy-basic-nft.js
@@ -1,5 +1,7 @@
 const { network } = require("hardhat")
 const { networkConfig, developmentChains } = require("../helper.hardhat.config")
+
+const { verify } = require("../utils/verify")
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
